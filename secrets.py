cb7a1e4991a829e9bf5f6332bddf3e5c3213a423
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-08-25 10:38:37 -0700

    add tda creds

--- secrets.py
+++ secrets.py
@@ -5,3 +5,9 @@ Credentials for Praw
 user_agent = "Comment Extraction"
 client_id = "QktIf3FZ10C5jg"
 client_secret = "52XN7-d-s7tBO8Wld9mPQ19PMaV8HA"
+
+"""
+Credentials for TDAmeritrade Developer API
+"""
+
+client_id = "8U8IWEYTVSA0S7UPF0K1N18HCBHAMF7A"
